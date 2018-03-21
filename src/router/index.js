fd7038d0bb448ed81770242c9880f0792e93b937
Jason(@CGQAQ) <m.jason.liu@outlook.com>
2018-03-21 13:28:23 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Welcome from '@/compoment/Welcome'
+import Welcome from '@/components/Welcome'
 
 Vue.use(Router)
 
@@ -9,13 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/index',
       name: 'welcome',
       component: Welcome
-    },
+    }
   ]
 })
